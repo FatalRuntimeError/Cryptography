ec8bdc12c68d4ef226553c14bc007f9fb1480590
GitHubEngine <nikita.nstu@gmail.com>
2019-09-04 21:59:33 +0700

    Lab1

---
+++ Lab1/Encoder/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyFile, txtFile, resultFile;
+            char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+
+            // Ввод имен файлов
+            Console.WriteLine("Введите название файла с ключом: ");
+            keyFile = Console.ReadLine();
+
+            Console.WriteLine("Введите название файла с текстом: ");
+            txtFile = Console.ReadLine();
+
+            Console.WriteLine("Введите название файла для записи результата: ");
+            resultFile = Console.ReadLine();
+
+            // Чтение ключа
+            char[] key = new char[33];
+            using (StreamReader stream = new StreamReader(keyFile, Encoding.UTF8))
+                key = stream.ReadToEnd().ToCharArray();
+
+            // Создание словаря для соответствий
+            Dictionary<char, char> accordance = new Dictionary<char, char>();
+            for (int i = 0; i < 33; i++)
+                accordance.Add(alphabet[i], key[i]);
+
+            // Чтение текста
+            string text;
+            using (StreamReader stream = new StreamReader(txtFile, Encoding.UTF8))
+                text = stream.ReadToEnd();
+
+            // Форматирование текста
+            StringBuilder formattedText = new StringBuilder();
+
+            text = text.ToLower();
+            foreach (var letter in text)
+                if (char.IsLetter(letter))
+                    formattedText.Append(letter);
+
+            // Кодирование текста
+            StringBuilder encodedText = new StringBuilder();
+             foreach (var letter in formattedText.ToString())
+                encodedText.Append(accordance[letter]);
+
+            // Запись закодированного текста
+            using (StreamWriter stream = new StreamWriter(resultFile, false, Encoding.UTF8))
+                stream.WriteLine(encodedText.ToString());
+
+        }
+    }
+}
---
+++ Lab1/KeyGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            char[] alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+
+            for (int i = 0; i < 33; i++)
+            {
+                int j = rand.Next(0, i);
+                char temp = alphabet[i];
+                alphabet[i] = alphabet[j];
+                alphabet[j] = temp;
+            }
+
+            Console.WriteLine("Введите имя файла для записи ключа:");
+            string filename = Console.ReadLine();
+
+            using (StreamWriter fstream = new StreamWriter(filename, false, Encoding.UTF8))
+                fstream.WriteLine(alphabet);
+        }
+    }
+}
