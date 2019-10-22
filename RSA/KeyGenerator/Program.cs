4a624e892ec73d716c655c0096e3614dfeddb195
Nikita <nikita.nstu@gmail.com>
2019-10-22 15:41:42 +0700

    RSA completed

--- RSA/KeyGenerator/Program.cs
+++ RSA/KeyGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace KeyGenerator
 {
@@ -28,6 +29,8 @@ namespace KeyGenerator
 
 			return isPrime;
 		}
+
+		static bool IsCoprime(int a, int b) => GCD(a, b) == 1;
 
 		static void ReadPQ(ref int p, ref int q)
 		{
@@ -59,13 +62,43 @@ namespace KeyGenerator
 			while (p == q);
 		}
 
+		static int ModInv(int e, int ln)
+		{
+			int t = 0, newt = 1;
+			int r = ln, newr = e;
+
+			while (newr != 0)
+			{
+				int q = r / newr;
+				(t, newt) = (newt, t - q * newt);
+				(r, newr) = (newr, r - q * newr);
+			}
+
+			return t < 0 ? t + ln : t;
+		}
+
 		static void Main(string[] args)
-		{ 
+		{
+			Random rand = new Random();
 			int p = 0, q = 0;
 			ReadPQ(ref p, ref q);
 
 			int n = p * q;
-			int lN = LCM(p - 1, q - 1);
+			int ln = LCM(p - 1, q - 1);
+
+			int e = rand.Next(2, ln);
+
+			while (!IsCoprime(e, ln))
+			{
+				e++;
+				if (e == ln)
+					e = rand.Next(2, ln);
+			}
+
+			int d = ModInv(e, ln);
+
+			File.WriteAllText("PublicKey.txt", string.Format("{0} {1}", n, e));
+			File.WriteAllText("PrivateKey.txt", string.Format("{0} {1}", n, d));
 		}
 	}
 }
