fd1b3d585379d810d50081c1787d0b110f689bb0
Nikita <nikita.nstu@gmail.com>
2019-11-24 10:23:34 +0700

    DigitalSignature not completed

---
+++ RSA/Encoder/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+
+namespace Encoder
+{
+	class Program
+	{
+		static ulong ModularAdd(ulong a, ulong b, ulong mod)
+		{
+			ulong result = 0;
+
+			a = a % mod;
+			b = b % mod;
+
+			ulong diff = mod > a ? mod - a : a - mod;
+
+			if (b < diff)
+				result = a + b;
+			else
+				result = b - diff;
+
+			return result % mod;
+		}
+
+		static ulong ModularMult(ulong a, ulong b, ulong mod)
+		{
+			ulong result = 0;
+			a = a % mod;
+			b = b % mod;
+
+			while (b != 0)
+			{
+				if (b % 2 == 1)
+					result = ModularAdd(result, a, mod);
+				a = ModularAdd(a, a, mod);
+				b >>= 1;
+			}
+
+			return result;
+		}
+
+		static ulong ModularPow(ulong b, ulong exp, ulong mod)
+		{
+			ulong result = 1;
+
+			while (exp > 0)
+			{
+				if (exp % 2 == 1)
+					result = ModularMult(result, b, mod);
+				exp >>= 1;
+				b = ModularMult(b, b, mod);
+			}
+
+			return result;
+		}
+
+		static void ParseKey(string filename, ref ulong mod, ref ulong exp)
+		{
+			string data = File.ReadAllText(filename);
+			string[] tokens = data.Split(' ');
+			mod = ulong.Parse(tokens[0]);
+			exp = ulong.Parse(tokens[1]);
+		}
+
+		static void EncodeMSG()
+		{
+			ulong mod = 0, exp = 0;
+			Console.Write("Enter file with public key: ");
+			string filename = Console.ReadLine();
+			ParseKey(filename, ref mod, ref exp);
+
+			Console.Write("Enter file with message: ");
+			filename = Console.ReadLine();
+			ulong m = ulong.Parse(File.ReadAllText(filename));
+
+			ulong c = ModularPow(m, exp, mod);
+			File.WriteAllText("EncodedMessage.txt", c.ToString());
+		}
+
+		static void DecodeMSG()
+		{
+
+			ulong mod = 0, exp = 0;
+			Console.Write("Enter file with private key: ");
+			string filename = Console.ReadLine();
+			ParseKey(filename, ref mod, ref exp);
+
+			Console.Write("Enter file with message: ");
+			filename = Console.ReadLine();
+			ulong m = ulong.Parse(File.ReadAllText(filename));
+
+			ulong c = ModularPow(m, exp, mod);
+			File.WriteAllText("DecodedMessage.txt", c.ToString());
+		}
+
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("1 - Encode message.");
+			Console.WriteLine("2 - Decode message.");
+			int choice = 0;
+			while (choice != 1 && choice != 2)
+			{
+				Console.Write("Your choice: ");
+				choice = int.Parse(Console.ReadLine());
+			}
+
+			if (choice == 1)
+				EncodeMSG();
+
+			if (choice == 2)
+				DecodeMSG();
+		}
+	}
+}
---
+++ RSA/KeyGenerator/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+
+namespace KeyGenerator
+{
+	class Program
+	{
+		static ulong GCD(ulong a, ulong b)
+		{
+			while (b != 0)
+			{
+				ulong t = b;
+				b = a % b;
+				a = t;
+			}
+
+			return a;
+		}
+
+		static ulong LCM(ulong a, ulong b) => a * b / GCD(a, b);
+
+		static bool IsPrime(ulong num)
+		{
+			bool isPrime = true;
+			ulong top = (ulong)Math.Sqrt(num) + 1;
+			for (ulong i = 2; i < top && isPrime; i++)
+				if (num % i == 0)
+					isPrime = false;
+
+			return isPrime;
+		}
+
+		static bool IsCoprime(ulong a, ulong b) => GCD(a, b) == 1;
+
+		static void ReadPQ(ref ulong p, ref ulong q)
+		{
+			do
+			{
+				do
+				{
+					Console.Write("Enter p: ");
+					p = ulong.Parse(Console.ReadLine());
+
+					if (!IsPrime(p))
+						Console.WriteLine("ERROR!. p is not prime number.\n");
+				}
+				while (!IsPrime(p));
+
+				do
+				{
+					Console.Write("Enter q: ");
+					q = ulong.Parse(Console.ReadLine());
+
+					if (!IsPrime(q))
+						Console.WriteLine("ERROR!. q is not prime number.\n");
+				}
+				while (!IsPrime(q));
+
+				if (p == q)
+					Console.WriteLine("ERROR! q equals p.\n");
+			}
+			while (p == q);
+		}
+
+		static ulong ModInv(ulong e, ulong ln)
+		{
+			ulong t = 0, newt = 1;
+			ulong r = ln, newr = e;
+
+			while (newr != 0)
+			{
+				ulong q = r / newr;
+				(t, newt) = (newt, t - q * newt);
+				(r, newr) = (newr, r - q * newr);
+			}
+
+			return t < 0 ? t + ln : t;
+		}
+
+		static void Main(string[] args)
+		{
+			// Read p and q and check if p and q are prime
+			ulong p = 0, q = 0;
+			ReadPQ(ref p, ref q);
+
+			// Compute n and lambda(n)
+			ulong n = p * q;
+			ulong ln = LCM(p - 1, q - 1);
+
+			// Generate e and check if e and lambda(n) are coprime
+			Random rand = new Random();
+			ulong e = (1 << 16) + 1;
+
+			if (e >= ln)
+			{
+				e = (ulong)rand.Next(2, (int)ln);
+				while (!IsCoprime(e, ln))
+				{
+					e++;
+					if (e == ln)
+						e = (ulong)rand.Next(2, (int)ln);
+				}
+			}
+
+
+			while (!IsCoprime(e, ln))
+				e--;
+
+			// Compute d
+			ulong d = ModInv(e, ln);
+
+			// Writing keys to files
+			File.WriteAllText("PublicKey.txt", string.Format("{0} {1}", n, e));
+			File.WriteAllText("PrivateKey.txt", string.Format("{0} {1}", n, d));
+		}
+	}
+}
